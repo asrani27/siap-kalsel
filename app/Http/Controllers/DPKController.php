29d8e14036a4272b598ccbab166c6f6c5dff0402
asrani <asrani.27@gmail.com>
2025-01-05 11:10:24 +0800

    f

--- app/Http/Controllers/DPKController.php
+++ app/Http/Controllers/DPKController.php
@@ -6,5 +6,8 @@ use Illuminate\Http\Request;
 
 class DPKController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('dpk.home');
+    }
 }
--- app/Http/Controllers/DPWController.php
+++ app/Http/Controllers/DPWController.php
@@ -6,5 +6,8 @@ use Illuminate\Http\Request;
 
 class DPWController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('dpw.home');
+    }
 }
