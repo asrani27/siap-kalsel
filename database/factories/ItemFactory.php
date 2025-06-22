bda46a071a2f1b8aa69c295da4ab88176ea849a5
asrani <asrani.27@gmail.com>
2025-06-22 19:34:53 +0800

    f

---
+++ database/factories/ItemFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\COA;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\COA>
+ */
+class ItemFactory extends Factory
+{
+
+    protected $model = COA::class;
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'kode' => $this->faker->unique()->regexify('[1-9]{1}\.[0-9]{1}(\.[0-9]{1})?'),
+        ];
+    }
+}
