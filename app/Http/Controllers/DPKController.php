799549af024f7c971876343997568d5d1b9ab7fc
asrani <asrani.27@gmail.com>
2026-02-28 09:16:22 +0800

    f

--- app/Http/Controllers/DPKController.php
+++ app/Http/Controllers/DPKController.php
@@ -1051,7 +1051,12 @@ class DPKController extends Controller
             ->groupBy('coa', 'coa_name')
             ->get();
 
-        $allKeuangans = Keuangan::where('user_id',  Auth::user()->id)->orderBy('created_at', 'asc')->get();
+        $allKeuangans = Keuangan::where('user_id',  Auth::user()->id)
+            ->whereBetween('created_at', [
+                $mulai . ' 00:00:00',
+                $sampai . ' 23:59:59'
+            ])
+            ->orderBy('created_at', 'asc')->get();
 
         // Hitung saldo secara akurat dengan iterasi dari awal
         $saldo = 0;
